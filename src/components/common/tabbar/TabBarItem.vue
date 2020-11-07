b945b2248ac2b219a44f24591d542500808e80b7
wkssr <250307471@qq.com>
2020-11-07 17:25:23 +0800

    vue.js items

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="tab-bar-item" @click="itemClick">
+    <div class="item-icon" v-show="!isActive"><slot name="icon"></slot></div>
+    <div class="item-active-icon" v-show="isActive"><slot name="active-icon"></slot></div>
+    <div class="item-text" :style="activeStyle"><slot name="text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red'
+    }
+  },
+data() {
+  return {
+    // isActive:false
+  }
+},
+computed: {
+  isActive() {
+    return this.$route.path.indexOf(this.path) !== -1
+  },
+  activeStyle() {
+    return this.isActive ? {color: this.activeColor} : {}
+  }
+},
+methods: {
+    itemClick() {
+     this.$router.replace(this.path)
+    
+    }
+  }
+}
+</script>
+
+<style>
+#tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+#tab-bar-item img {
+  height: 24px;
+  width: 24px;
+  margin-top: 3px;
+    /* 去除图片底部默认像素 */
+  vertical-align: middle;  
+  margin-bottom: 2px;
+
+}
+.active {
+  color: red;
+}
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="home">
+  <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from 'common/navbar/NavBar'
+import {getHomeMultidata} from "network/home"
+export default {
+name: "Home",
+components: {
+  NavBar,
+  created() {
+    //1.请求多个数据
+    getHomeMultidata().then(res => {
+      console.log(res);
+    })
+  },
+}
+
+}
+</script>
+
+<style>
+
+</style>
