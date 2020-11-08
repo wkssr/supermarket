c8200301bb7107e0d9492e59ec82cb7bdeadeb55
wkssr <250307471@qq.com>
2020-11-08 13:43:41 +0800

    vue.js items

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,23 +1,43 @@
 <template>
 <div id="home">
   <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  <swiper>
+    <swiper-item v-for="(item,index) in banners" :key="index">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </swiper-item>
+  </swiper>
   </div>
 </template>
 
 <script>
-import NavBar from 'common/navbar/NavBar'
-import {getHomeMultidata} from "network/home"
+import NavBar from 'common/navbar/NavBar';
+import {getHomeMultidata} from "network/home";
+import {Swiper, SwiperItem} from 'common/swiper/index.js';
 export default {
 name: "Home",
 components: {
   NavBar,
+  Swiper,
+  SwiperItem
+},
+data() {
+  return {
+    banners: [],
+    recommends: []
+  }
+},
   created() {
     //1.请求多个数据
     getHomeMultidata().then(res => {
       console.log(res);
+      //避免变量被垃圾回收
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+
     })
-  },
-}
+  }
 
 }
 </script>
